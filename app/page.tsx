12a22fb5672f430403108da426e1c308c0b24699
polenov_dv <polenovdimaprok@gmail.com>
2024-03-19 22:30:17 +0300

    first commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,95 +1,209 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+"use client";
+import { useState, useEffect } from "react";
+import Web3 from "web3";
+import { makeStyles } from "@mui/styles";
+import {
+  Button,
+  Container,
+  TextField,
+  Typography,
+  Select,
+  MenuItem,
+  Box,
+  Grid,
+} from "@mui/material";
+
+const useStyles = makeStyles(() => ({
+  content: {
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+    backgroundColor: "#d1d7ed",
+    height: "100vh",
+  },
+  container: {
+    padding: "40px",
+    backgroundColor: "#fff",
+    borderRadius: "20px",
+  },
+}));
 
 export default function Home() {
+  const cls = useStyles();
+  const [web3, setWeb3] = useState<Web3 | null>(null);
+  const [account, setAccount] = useState<string>("");
+  const [ethBalance, setEthBalance] = useState<string>("");
+  const [bnbBalance, setBnbBalance] = useState<string>("");
+  const [recipientAddress, setRecipientAddress] = useState<string>("");
+  const [network, setNetwork] = useState<string>("mainnet");
+  const [transferAmount, setTransferAmount] = useState<string>("");
+  const [walletType, setWalletType] = useState<string>("eth");
+  const [disableWalletSelect, setDisableWalletSelect] =
+    useState<boolean>(false);
+
+  useEffect(() => {
+    const initWeb3 = async () => {
+      // @ts-ignore
+      const ethereum = window.ethereum;
+      if (ethereum) {
+        const web3Instance = new Web3(ethereum);
+        setWeb3(web3Instance);
+
+        try {
+          await ethereum.request({ method: "eth_requestAccounts" });
+
+          const accounts = await web3Instance.eth.getAccounts();
+          setAccount(accounts[0]);
+
+          const ethBalance = await web3Instance.eth.getBalance(accounts[0]);
+          setEthBalance(web3Instance.utils.fromWei(ethBalance, "ether"));
+
+          const bnbBalance = await web3Instance.eth.getBalance(accounts[0]);
+          setBnbBalance(web3Instance.utils.fromWei(bnbBalance, "ether"));
+        } catch (error) {
+          console.error(error);
+        }
+      }
+    };
+
+    initWeb3();
+  }, []);
+
+  const handleTransaction = async () => {
+    if (!web3 || !recipientAddress || !transferAmount) return;
+
+    const amountInWei = web3.utils.toWei(transferAmount, "ether");
+    if (!disableWalletSelect) {
+      const balanceInWei =
+        walletType === "eth"
+          ? web3.utils.toWei(ethBalance, "ether")
+          : web3.utils.toWei(bnbBalance, "ether");
+
+      if (parseInt(amountInWei) > parseInt(balanceInWei)) {
+        console.error("Insufficient balance");
+        return;
+      }
+    }
+
+    try {
+      const tx = await web3.eth.sendTransaction({
+        from: account,
+        to: recipientAddress,
+        value: amountInWei,
+      });
+      console.log("Transaction successful:", tx);
+    } catch (error) {
+      console.error("Transaction failed:", error);
+    }
+  };
+
+  const handleNetworkChange = async (value: string) => {
+    setNetwork(value);
+    setDisableWalletSelect(value === "testnet");
+
+    if ("ethereum" in window) {
+      try {
+        await (window as any).ethereum.request({
+          method: "wallet_switchEthereumChain",
+          params: [{ chainId: value === "mainnet" ? "0x1" : "0x3" }],
+        });
+      } catch (error) {
+        console.error(error);
+      }
+    }
+  };
+
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>app/page.tsx</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
+    <main className={cls.content}>
+      <Container className={cls.container} maxWidth="md">
+        <Typography variant="h2" align="center">
+          Wallet
+        </Typography>
+        <Typography variant="h5" gutterBottom>
+          Address:
+          <Box
+            component="span"
+            display="inline"
+            color="olive"
+            sx={{ marginLeft: "5px", overflowWrap: "break-word" }}
           >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
+            {account}
+          </Box>
+        </Typography>
+        <Typography variant="h6" gutterBottom>
+          ETH Balance:
+          <Box component="span" color="blue" margin={1}>
+            {ethBalance}
+          </Box>
+          ETH
+        </Typography>
+        <Typography variant="h6" style={{ marginBottom: "2rem" }}>
+          BNB Balance:
+          <Box component="span" color="blue" margin={1}>
+            {bnbBalance}
+          </Box>
+          BNB
+        </Typography>
+        <Select
+          value={network}
+          onChange={(event) => handleNetworkChange(event.target.value)}
+          fullWidth
+          variant="outlined"
+          style={{ marginBottom: "1rem" }}
+        >
+          <MenuItem value="mainnet">Mainnet</MenuItem>
+          <MenuItem value="testnet">Testnet</MenuItem>
+        </Select>
+
+        <Grid container spacing={1} alignItems="center">
+          <Grid item xs>
+            <TextField
+              fullWidth
+              label="Amount"
+              variant="outlined"
+              type="number"
+              value={transferAmount}
+              onChange={(e) => setTransferAmount(e.target.value)}
+              inputProps={{
+                min: "0",
+                max: walletType === "eth" ? ethBalance : bnbBalance,
+                step: "0.01",
+              }}
+              style={{ marginBottom: "1rem" }}
             />
-          </a>
-        </div>
-      </div>
+          </Grid>
 
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
+          <Grid item>
+            <Select
+              value={walletType}
+              disabled={disableWalletSelect}
+              onChange={(e) => setWalletType(e.target.value as string)}
+              variant="outlined"
+              style={{ marginBottom: "1rem" }}
+            >
+              <MenuItem value="eth">ETH</MenuItem>
+              <MenuItem value="bnb">BNB</MenuItem>
+            </Select>
+          </Grid>
+        </Grid>
+
+        <TextField
+          fullWidth
+          label="Recipient Address"
+          variant="outlined"
+          value={recipientAddress}
+          onChange={(e) => setRecipientAddress(e.target.value)}
+          style={{ marginBottom: "1rem" }}
         />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
+        <Button
+          variant="contained"
+          color="primary"
+          onClick={handleTransaction}
+          style={{ padding: "10px" }}
         >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore starter templates for Next.js.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
+          Send Transaction
+        </Button>
+      </Container>
     </main>
   );
 }
